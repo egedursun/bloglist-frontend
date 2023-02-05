028c30e672d29d776640c2c87b823bd4a141af56
egedursun <edogandursun@gmail.com>
2023-02-05 16:48:12 +0300

    implemented the changes for the part5

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,35 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+let token = null
+let config
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+  config = {
+    headers: { Authorization: token },
+  }
 }
 
-export default { getAll }
+const getAll = async () => {
+  const response = await axios.get(baseUrl, config)
+  return response.data
+}
+
+const create = async newObject => {
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+const update = async objectToUpdate => {
+  const response = await axios.put(`${baseUrl}/${objectToUpdate.id}`, objectToUpdate, config)
+  return response.data
+}
+
+const remove = async id => {
+  const response = await axios.delete(`${baseUrl}/${id}`, config)
+  return response.data
+}
+
+
+export default { getAll, create, update, setToken, remove }
